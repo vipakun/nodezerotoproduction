97def17c5ff896d9a18a3ce5e3abb71cba33d1a3
Xiangkun Yang <vipakun@hotmail.com>
2016-08-26 16:26:56 -0500

    Init

---
+++ src/server/test/models.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const assert = require('chai').assert;
+const Sequelize = require('sequelize');
+const db = require('../models/db');
+const Todo = require('../models/todo');
+
+// reset the test database
+before(function(done) {
+  db.drop().then(function() {
+    return db.sync({ force: true });
+  }).then(function() {
+    done();
+  }).catch(function(err) {
+    console.log(err);
+  });
+});
+
+describe('The Todo Model', function() {
+
+  it('should accept text for the TODO item', function() {
+    let todoText = 'this is a super todo';
+    return Todo.create({
+      title: todoText
+    }).then(function(todo) {
+      assert.equal(todo.title, todoText, 'Saved text should match');
+    });
+  });
+
+});
